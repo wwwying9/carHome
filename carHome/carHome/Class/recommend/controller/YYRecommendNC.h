6367b1b3a9128296ab58eec902a96d1145e40f69
yaoying <yaoying@520it.com.cn>
2016-05-19 17:21:33 +0800

    搭建recommend的基础框架

---
+++ carHome/carHome/Class/recommend/controller/YYRecommendNC.h
@@ -0,0 +1,13 @@
+//
+//  YYRecommendNC.h
+//  carHome
+//
+//  Created by 姚英 on 16/5/19.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYRecommendNC : UINavigationController
+
+@end
