e9df0e8aebe39598046d5778c090776f3d3e3a1d
yaoying <yaoying@520it.com.cn>
2016-05-19 09:17:19 +0800

    创建工程,初始化pod

---
+++ carHome/carHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  carHome
+//
+//  Created by 姚英 on 16/5/19.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ carHome/carHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  carHome
+//
+//  Created by 姚英 on 16/5/19.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
